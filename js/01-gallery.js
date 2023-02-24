8617e450746a58e1c6ecf32ab958fb23ede7ba73
bolt2000 <bolt2000@ukr.net>
2023-02-24 20:59:46 +0200

    1+

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const div = document.querySelector('.gallery')
+
+const cardsMarkup = creatImageCards(galleryItems);
+div.insertAdjacentHTML('beforeend', cardsMarkup);
+div.addEventListener('click', onDivClick);
+
+
+function creatImageCards(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+       <li>
+       <img
+       class="gallery__image"
+       href="${original}"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+       >
+       </li>
+        `;
+    }).join('');
+}
+
+
+function onDivClick(e) {
+    e.preventDefault();
+
+    const imageBigSize = e.target.classList.contains('gallery__image');
+
+    if (!imageBigSize) {
+        return;
+    }
+const instance = basicLightbox.create(`
+    <h1 class="word">${e.target.alt}</h1>
+	<img src="${e.target.dataset.source}">
+`).show();
+    
+};
+
+
